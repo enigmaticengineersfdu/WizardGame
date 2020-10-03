4c10f48ebf3fef608dd7124c95fd41b51ab6e5fb
Falaksher Chaudhary <Falaksher5@outlook.com>
2020-10-03 17:58:39 -0400

    Added a menu in UI

--- WizardGame/ui.cpp
+++ WizardGame/ui.cpp
@@ -1,6 +1,26 @@
 #include "ui.h"
+#include <iostream>
+#include <sstream>
 
 std::string render()
 {
-	// This will print 
+	// This will print the start screen
+
+	int choice;
+	
+	std::cout << "Welcome to Apostate!\nPick an option:\n" << std::endl;
+
+	std::cout << "1) Start\n2) Exit" << std::endl;
+
+	std::cin >> choice;
+
+	switch (choice)
+		case 1:
+
+			break;
+		case 2:
+			exit;
+		default:
+			std::cout << "Sorry, incorrect input" << std::endl;
+
 }
