05d881ebbc9920f397be9fd1700663b13daac3d6
Mohit Dhiren Patel <lead999x@gmail.com>
2020-10-02 22:29:53 -0400

    Started implementing the EntityMatrix. Item and Character classes will need reworking to work properly with it.

--- WizardGame/entitymgmt.h
+++ WizardGame/entitymgmt.h
@@ -2,21 +2,30 @@
 #define ENTITYMGMTH
 
 #include <unordered_map>
-#include <unordered_set>
+#include <queue>
 
+#include "gameloop.h"
 #include "entities.h"
+#include "map.h"
 
 namespace ent {
         /**Entity Management Classes**/
         class EntityMatrix
         {
         private:
-                std::unordered_map<ItemID, Item> item_table;
-                std::unordered_set<ItemID> item_id_pool;
-                std::unordered_map<CharacterID, Enemy> character_table;
-                std::unordered_set<CharacterID> character_id_pool;
+                std::unordered_map<ItemID, Item>       item_table;       //Contains all items in play.
+                std::queue<ItemID>                     item_id_pool;     //Contains all available ItemIDs.
+                std::unordered_map<CharacterID, Enemy> character_table;  //Contains all enemies in play.
+                std::queue<CharacterID>                character_id_pool;//Contains all available CharacterIDs.
                 Player player;
 
+                /*Purpose: To add more ids to the id pools if they become empty.
+                * Preconditions: Either of the id pools is empty.
+                * Postconditions: The empty id pool(s) are replenished with 10 ids.
+                * Throws: Does not throw.
+                * Note: Meant to be called by member functions of this class that create new entities.
+                */
+                void replenish_id_pools() noexcept;
         public:
                 /*Purpose: To default construct objects of this type.
                 * Preconditions: None.
@@ -24,12 +33,48 @@ namespace ent {
                 * Throws: Does not throw.
                 */
                 EntityMatrix() noexcept;
+
+                /*Purpose: To assign a unique ItemID to an item.
+                * Preconditions: The calling object must be valid.
+                * Postconditions: A unique ItemID is returned.
+                * Throws: None.
+                * Note: Meant to be called in the constructor of an Item. 
+                */
+                const ItemID request_item_id() noexcept;
+                /*Purpose: Reclaims the ItemID of an Item that is being removed from play.
+                * Preconditions: The Item whose ID is to be reclaimed is being removed from play this tick.
+                * and will not exist in the next tick. 
+                * Throws: None.
+                * Note: Meant to be called in the tick function of an Item. (Not in the destructor!!!)
+                */
+                void reclaim_item_id(const ItemID id) noexcept;
+                /*Purpose: To assign a unique CharacterID to a Character.
+                * Preconditions: The calling object must be valid.
+                * Postconditions: A unique CharacterID is returned.
+                * Throws: None.
+                * Note: Meant to be called in the constructor of a Character.
+                */
+                const CharacterID request_character_id() noexcept;
+                /*Purpose: Reclaims the CharacterID of a Character that is being removed from play.
+                * Preconditions: The Character whose ID is to be reclaimed is being removed from play this tick.
+                * and will not exist in the next tick.
+                * Throws: None.
+                * Note: Meant to be called in the tick function of a Character. (Not in the destructor!!!)
+                */
+                void reclaim_character_id(const ItemID &id) noexcept;
+                /*Purpose: To create the next iteration of the EntityMatrix.
+                * Preconditions: The calling object is valid and the player is alive. 
+                * Postconditions: The next iteration of the EntityMatrix is created.
+                * Note: Meant to be called in the handler functions of the game loop.
+                * Internally calls the tick functions of all contained entities.
+                */
+                std::optional<EntityMatrix> generate_next(const gl::Input input) const;
         };
 
         struct GameState
         {
-                EntityMatrix entity_matrix;
-                //Map         map;
+                EntityMatrix entity_matrix;//The entity matrix contains the state of all entities in play.
+                Map          map;          //Contains map state.
 
                 /*Purpose: To serialize the current game state to a file on disk.
                 * Preconditions: The calling object is valid.
