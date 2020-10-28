56be7f74ddbf6f8df3a881c91cc2dab9a9a4d912
Trinity Headen <trinityah@gmail.com>
2020-10-28 18:49:16 -0400

    Removed test.cpp from project

--- WizardGame/test.cpp
+++ WizardGame/test.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include "curses.h"
-
-int main(int argv, char* argc[])
-{
-        WINDOW* win = initscr();
-        int resc = waddch(win, 'A');
-        int res = flash();
-        std::cout << resc << " " << res << "\n";
-        //menu loop
-       /* while (true) {
-
-        }*/
-        return 0;
-}
+//#include <iostream>
+//#include "curses.h"
+//
+//int main(int argv, char* argc[])
+//{
+//        WINDOW* win = initscr();
+//        int resc = waddch(win, 'A');
+//        int res = flash();
+//        std::cout << resc << " " << res << "\n";
+//        menu loop
+//       /* while (true) {
+//
+//        }*/
+//        return 0;
+//}
